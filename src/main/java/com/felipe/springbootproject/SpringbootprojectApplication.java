0ae083bc26dd5a86ca723480fd3c48275a44e104
Felipe de Azevedo <felipe.deazevedo@outlook.com>
2022-08-17 15:16:37 -0300

    Initial commit

---
+++ src/main/java/com/felipe/springbootproject/SpringbootprojectApplication.java
@@ -0,0 +1,30 @@
+package com.felipe.springbootproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.felipe.springbootproject.entities.Category;
+import com.felipe.springbootproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class SpringbootprojectApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+	}
+
+}
